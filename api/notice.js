8f89dac4262c9951be68943c15713ee979775625
雾山 <li997477295@outlook.com>
2021-03-05 14:59:37 +0800

    22
    222222

--- api/notice.js
+++ api/notice.js
@@ -5,16 +5,16 @@ module.exports = async (secret, title, body) => {
 
   let config = {
     method: 'post',
-    url: `http://sc.ftqq.com/${secret}.send`,
+    url: `http://sctapi.ftqq.com/${secret}.send`,
     headers: {
-      Host: 'sc.ftqq.com',
+      Host: 'sctapi.ftqq.com',
       Accept: '*/*',
       'X-Requested-With': 'XMLHttpRequest',
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      Origin: 'http://sc.ftqq.com',
-      Referer: 'http://sc.ftqq.com/?c=code',
+      Origin: 'http://sctapi.ftqq.com',
+      Referer: 'http://sctapi.ftqq.com/?c=code',
       'Accept-Language': 'zh-CN,zh;q=0.9',
       Cookie: 'PHPSESSID=0c77175e2c4ebd7323b0c3d5f5439e3b',
     },
